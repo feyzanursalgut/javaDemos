c41b66efabb6adf02302b1a9abdf0bdc9249f8cf
Feyza <60342829+feyzanursalgut@users.noreply.github.com>
2022-09-18 19:29:13 +0300

    Add files via upload

---
+++ dataTypes/dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,15 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double sayi=12.5;
+		sayi=-129;
+
+		char karakter='A';
+		boolean dogruMu=false;
+
+	}
+
+}
